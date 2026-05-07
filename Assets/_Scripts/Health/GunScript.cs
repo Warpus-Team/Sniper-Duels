110044f524d68bbe1a0ae8b37edec8f6fae1e928
Eric Barradas Baptista Dos Santos <emolgaex19@gmail.com>
2026-05-07 11:13:01 -0300

    Gun damage

--- Assets/_Scripts/Health/GunScript.cs
+++ Assets/_Scripts/Health/GunScript.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class GunScript : NetworkBehaviour
 {
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private LayerMask hitLayer;
+    [SerializeField] private float range = 20f;
+    [SerializeField] private int damage = 10;
+
     protected override void OnSpawned()
     {
         base.OnSpawned();
@@ -14,6 +19,15 @@ public class GunScript : NetworkBehaviour
 
     private void Update()
     {
-        
+        if (!Input.GetKeyDown(KeyCode.Mouse0))
+            return;
+
+        if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out var hit, range, hitLayer))
+            return;
+        if(!hit.transform.TryGetComponent(out PlayerHealth playerHealth))
+            return;
+
+        playerHealth.ChangeHealth(-damage);
+        Debug.Log($"Hit: {hit.transform.name}");
     }
 }
