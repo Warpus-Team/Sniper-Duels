e938e456109e55f5f7e81e09e5c8d7b3431e6a53
Eric Barradas <emolgaex19@gmail.com>
2026-05-23 21:26:23 -0300

    Add Scripts de Canvas View Manager

---
+++ Assets/_Scripts/UIUX/MainGameView.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameView : View
+{
+    public override void OnHide()
+    {
+
+    }
+
+    public override void OnShow()
+    {
+
+    }
+}
